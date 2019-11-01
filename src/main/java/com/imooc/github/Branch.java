39dcf1cb29f78a9c6cd730b8d31c518b573478aa
DarkSouls_lzy <1160827860@qq.com>
2019-11-01 23:35:47 +0800

    解决冲突
    解决代码冲突问题

--- src/main/java/com/imooc/github/Branch.java
+++ src/main/java/com/imooc/github/Branch.java
@@ -2,6 +2,6 @@ package com.imooc.github;
 
 public class Branch {
     public static void main(String[] args) {
-        System.out.println("测试冲突解决");
+        System.out.println("切换了输出语句");
     }
 }
